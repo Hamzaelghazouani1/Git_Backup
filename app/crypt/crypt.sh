3c9c4f461c44716a74c3bb2156a6a0f229e2be48
BENHIMA Mohamed-amine <benhima.mohamed.amine@gmail.com>
2024-05-10 09:07:05 +0100

    edit the crypt script read the comments to understand

--- app/crypt/crypt.sh
+++ app/crypt/crypt.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
-
-# how to use this script:
-
-# we have two functions in this script, one to encrypt a zipped folder and another to decrypt an encrypted file
-# to use the encrypt function, you need to pass the zip file and the password as arguments
-# to use the decrypt function, you need to pass the encrypted file in this format : encrypted_zippedFileName 
-# and the password as arguments
-
 # Function to encrypt a zipped folder using AES
 encrypt() {
     # Check if number of arguments is correct
-    if [ "$#" -ne 2 ]; then
-        echo "Usage: encrypt <zip_file> <password>"
+    if [ "$#" -ne 3 ]; then
+        echo "Usage: encrypt <zip_file> <output_enc_file> <password>"
         return 1
     fi
 
-    zip_file="$1"
-    password="$2"
-    encrypted_file="encrypted_${zip_file}"
+    zip_file="$1"           # Path to the zip file to be encrypted
+    output_enc_file="$2"    # Path where the encrypted file will be saved
+    password="$3"           # Password for encryption
 
     # Check if zip file exists
     if [ ! -f "$zip_file" ]; then
@@ -27,26 +19,26 @@ encrypt() {
     fi
 
     # Encrypt the zip file using AES
-    openssl enc -aes-256-cbc -salt -pbkdf2 -in "$zip_file" -out "$encrypted_file" -k "$password"
+    openssl enc -aes-256-cbc -salt -pbkdf2 -in "$zip_file" -out "$output_enc_file" -k "$password"
     if [ $? -ne 0 ]; then
         echo "Error: Encryption failed."
         return 1
     fi
 
-    echo "Encryption completed. Encrypted file: $encrypted_file"
+    echo "Encryption completed. Encrypted file: $output_enc_file"
 }
 
 # Function to decrypt an encrypted file using AES
 decrypt() {
     # Check if number of arguments is correct
-    if [ "$#" -ne 2 ]; then
-        echo "Usage: decrypt <encrypted_file> <password>"
+    if [ "$#" -ne 3 ]; then
+        echo "Usage: decrypt <encrypted_file> <output_zip_file> <password>"
         return 1
     fi
   
-    encrypted_file="$1"
-    password="$2"
-    decrypted_file="decrypted_$(basename "$encrypted_file" .enc)"
+    encrypted_file="$1"         # Path to the encrypted file
+    output_zip_file="$2"        # Path where the decrypted file will be saved
+    password="$3"               # Password for decryption
 
     # Check if encrypted file exists
     if [ ! -f "$encrypted_file" ]; then
@@ -55,18 +47,17 @@ decrypt() {
     fi
 
     # Decrypt the encrypted file using AES
-    openssl enc -d -aes-256-cbc -pbkdf2 -in "$encrypted_file" -out "$decrypted_file" -k "$password"
+    openssl enc -d -aes-256-cbc -pbkdf2 -in "$encrypted_file" -out "$output_zip_file" -k "$password"
     if [ $? -ne 0 ]; then
         echo "Error: Decryption failed."
         return 1
     fi
 
-    echo "Decryption completed. Decrypted file: $decrypted_file"
+    echo "Decryption completed. Decrypted file: $output_zip_file"
 }
 
 # Example usage
 # Encrypting a zipped folder
-# encrypt "test.zip" "20030629"
-
+# encrypt "/home/med-amine/Desktop/Git_Backup-main/test.zip" "/home/med-amine/Desktop/test.enc" "20030629"
 # Decrypting an encrypted file
-# decrypt "encrypted_test.zip" "20030629"
+# decrypt "/home/med-amine/Desktop/test.enc" "/home/med-amine/Desktop/decrypted_test.zip" "20030629"
